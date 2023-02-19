d06f200ddde0a54d2d42218f9db6f70914e126b1
Nosiphiwo <phiwo041590@gmail.com>
2023-02-19 09:42:21 -0800

    101 quots

---
+++ 101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A C programm that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+return (1);
+}
