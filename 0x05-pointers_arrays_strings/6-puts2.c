738748c7167006942458168ebf262562a2f028dc
Nosiphiwo <phiwo041590@gmail.com>
2023-02-27 09:34:49 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,17 +13,17 @@ void puts2(char *str)
 	char *y = str;
 	int o;
 
-	while (*y != '\n')
+	while (*y != '\0')
 	{
 		y++;
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0; <= t; o++)
+	for (o = 0; o <= t; o++)
 	{
 		if (o % 2 == 0)
 		{
-		-putchar(str[o]);
+		_putchar(str[o]);
 		}
 	}
 	_putchar('\n');
