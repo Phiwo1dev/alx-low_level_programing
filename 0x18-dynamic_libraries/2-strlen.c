fb758fb179d96a373b4553efdd70ae8fe5fd12dd
Phiwo Njemla <phiwo041590@gmail.com>
2023-08-21 14:28:53 -0700

    0. A library is not a luxury

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen - returns the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+int _strlen(char *s)
+
+{
+	int longi = 0;
+
+	while (s[longi])
+	{
+	longi++;
+	}
+	return (longi);
+}
