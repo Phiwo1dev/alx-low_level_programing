36224cece031feab3b24ca4be73dd1e8e1c42c00
Phiwo Njemla <phiwo041590@gmail.com>
2023-09-08 12:48:15 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creates a hash table.
+ * @size: size of the array.
+ *
+ * Return: NULL for error
+ *         Otherwise - pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+
+{
+	hash_table_t *ht;
+	unsigned long int h;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (h = 0; h < size; h++)
+		ht->array[h] = NULL;
+
+	return (ht);
+}
